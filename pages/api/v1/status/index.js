06c9981a0fe7830f2756e7cd7f5cb9e26001770b
Felipe Dias <faadias@users.noreply.github.com>
2025-01-21 17:20:05 +0000

    finaliza endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+
+  const dbVersionResult = await database.query("show server_version");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbOpenConnectionsResult = await database.query({
+    text: "SELECT count(*) open_connections FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  const dbOpenConnections = dbOpenConnectionsResult.rows[0].open_connections;
+
   response.status(200).json({
-    foo: "bar",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        open_connections: parseInt(dbOpenConnections),
+      },
+    },
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,29 @@
-test("GET to /api/v1/status deve retornar 200", async () => {
+test("GET /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+});
+
+test("GET /api/v1/status deve retornar um objeto com `updated_at` com uma data válida", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("GET /api/v1/status deve retornar um objeto dependencies", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.dependencies).toBeDefined();
+});
+
+test("GET /api/v1/status deve retornar um objeto database com `version`, `max_connections` e `open_connections`", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  const dependencies = responseBody.dependencies;
+  expect(dependencies.database.version).toMatch(/^[1-9]+[.][0-9]+$/);
+  expect(dependencies.database.max_connections).toBeGreaterThan(0);
+  expect(dependencies.database.open_connections).toBe(1);
 });
